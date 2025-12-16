b1343b908835dc626381e2e3adcc6a8683123d7c
PauMorillas <morillashuertapau@gmail.com>
2025-12-16 16:51:55 +0100

    (Feat) Metodos CRUD de negocio con su respectiva respuesta HTTP y su separación con la lógica de negocio en el Service

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -1,13 +1,41 @@
 <?php 
 namespace App\Http\Controllers;
 
+use App\Models\Business;
+use Illuminate\Http\Request;
 use App\Services\BusinessService;
 use Illuminate\Routing\Controller;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Js;
+use Nette\Utils\Json;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class BusinessController extends Controller {
 
-    // route('business/{id}')
-    public function findById($id){
-        return BusinessService::findById($id);
+    public function findById(int $id): JsonResponse {
+        $business = BusinessService::findById($id);
+
+        return response()->json([$business], 200);
+    }
+
+    public function create(Request $request): JsonResponse{
+        $data = $request->all();
+
+        BusinessService::create($data);
+
+        return response()->json(['created' => true], 201);
+    }
+
+    public function update(int $id, Request $request): JsonResponse {
+        $data = $request->all();
+        $business = BusinessService::update($id, $data);
+
+        return response()->json([$business], 200);
+    }
+
+    public function delete(int $id): JsonResponse {
+        BusinessService::delete($id);
+
+        return response()->json(['deleted' => true], 204);
     }
 }
--- app/Services/BusinessService.php
+++ app/Services/BusinessService.php
@@ -3,8 +3,24 @@ namespace App\Services;
 
 use App\Models\Business;
 
-    class BusinessService{
+    class BusinessService {
         public static function findById($id){
-            return Business::find($id);
+            return Business::findOrFail($id);
+        }
+        
+        public static function create($data){
+            return Business::create($data);
+        }
+
+        public static function update($id, $data){
+            $business = Business::findOrFail($id);
+            $business->update($data);
+            return $business;
+        }
+
+        public static function delete($id){
+            $business = Business::findOrFail($id); // Si falla, lanza una excepción 404 automáticamente 
+            $business->delete();
+            return true; // Por lo que podemos asumir que si llega aqui es porque se eliminó con exito
         }
     }
